f5715398fa969048f5f606c330a52960c1ccf9a7
Phuong-Dong-Le <65102169+phuongle0701@users.noreply.github.com>
2021-04-03 22:36:24 -0400

    Add files via upload

--- matlab/gen_data_KdV.m
+++ matlab/gen_data_KdV.m
@@ -1,14 +1,16 @@
 %% Korteweg-de Vries equation (two-soliton solution)
-nn = 512;
+clear all; 
+clc; 
+format short;
+nn = 100;
 steps = 200;
-
-dom = [-20 20]; x = chebfun('x',dom); 
-tspan = linspace(0,40,steps+1);
+tmax = 5;
+dom = [-8 8]; x = chebfun('x',dom); tspan = linspace(0,tmax,steps+1);
 S = spinop(dom, tspan);
 S.lin = @(u) -1.0*diff(u,3);
 S.nonlin = @(u) -1.0*0.5*diff(u.^2); % spin cannot parse "u.*diff(u)"
-%S.init = -sin(pi*x/20);
-S.init = cos(-1*(pi*x/20)); 
+%S.init = -sin(pi*x/8);
+S.init = cos(-pi*x/8);
 u = spin(S,nn,1e-4);
 
 usol = zeros(nn,steps+1);
@@ -16,20 +18,20 @@ for i = 1:201
     usol(:,i) = u{i}.values;
 end
 
-x = linspace(-20,20,nn+1);
+x = linspace(-8,8,nn+1);
 x = x(1:end-1);
 t = tspan;
-figure(2); 
-pcolor(t,x,usol); 
-shading interp; 
-axis tight; 
-colormap(jet);
+figure(2)
+pcolor(t,x,usol); shading interp, axis tight, colormap(jet);
 xlabel('$t$', 'Interpreter', 'Latex'); 
 ylabel('$x$', 'Interpreter', 'Latex');
 colorbar; 
-%title('Exact Dynamics with $u_{0}(x) = - \sin(\frac{\pi x}{20})$', 'Interpreter', 'Latex'); 
-title('Exact Dynamics with $u_{0}(x) = \cos(\frac{-\pi x}{20})$', 'Interpreter', 'Latex'); 
+%title('Exact Dynamics with $u_{0}(x) = - \sin(\frac{\pi x}{8})$', 'Interpreter', 'Latex'); 
+title('Exact Dynamics with $u_{0}(x) = \cos( \frac{-\pi x}{8}) $', 'Interpreter', 'Latex'); 
 
-% save the result: 
-%save('KdV_sine.mat','t','x','usol')
-save('kdV_cosine.mat', 't', 'x', 'usol'); 
+%save('kdV_sine.mat','t','x','usol')
+save('kdV_cosine.mat','t','x','usol')
+
+
+
+
